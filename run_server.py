8dd1c51fcc6dade652c76a6bfdeb6fdd1e25f1b6
Yomel Barreto <jairbarretoflores138@gmail.com>
2026-02-17 02:26:37 -0500

    Fix: Render Timeout

--- run_server.py
+++ run_server.py
@@ -3,4 +3,5 @@ import os
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("src.api.server:app", host="0.0.0.0", port=port, reload=True)
+    # En produccion, quitamos reload=True para evitar cuelgues
+    uvicorn.run("src.api.server:app", host="0.0.0.0", port=port, reload=False)
